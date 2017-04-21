8653a36bf64767a74fd5eb1ca64e88c756500f5c
Christos <chris.paraskeva10@gmail.com>
2017-04-21 15:41:49 +0100

    test pass / game assign X to player1

--- spec/models/gameSpec.js
+++ spec/models/gameSpec.js
@@ -12,4 +12,8 @@ describe('Game', function() {
   it("receives a player 2", function() {
       expect(game.player2 instanceof Player).toBe(true);
   });
+
+  it("player1 is initialized with 'X'", function() {
+    expect(game.player1.symbol).toEqual('X');
+  });
 });
--- src/models/game.js
+++ src/models/game.js
@@ -1,6 +1,6 @@
 (function(exports) {
 
-  function Game(player1 = new Player(), player2 = new Player()) {
+  function Game(player1 = new Player('X'), player2 = new Player()) {
     this.player1 = player1;
     this.player2 = player2;
     console.log(this.player1 === this.player1)
