014a775c031e51afc2d5d618eb72f080573e16bc
Christos <chris.paraskeva10@gmail.com>
2017-04-21 15:40:21 +0100

    test pass / player can be initalized with "O"

--- spec/models/playerSpec.js
+++ spec/models/playerSpec.js
@@ -5,8 +5,13 @@ describe('Player', function() {
     expect(player).toBeDefined();
   });
 
-  it("player1 is initialized with 'X'", function() {
+  it("player1 can be initialized with 'X'", function() {
     var player = new Player('X');
     expect(player.symbol).toEqual('X');
+  });
+
+  it("player can be initialized with 'O'", function() {
+    var player = new Player('O');
+    expect(player.symbol).toEqual('O');
   });
 });
