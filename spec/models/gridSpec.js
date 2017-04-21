3334885a53c6931d47085f04cdfff953aeba9918
Christos <chris.paraskeva10@gmail.com>
2017-04-21 16:56:53 +0100

    grid contains grid structure array

--- spec/models/gridSpec.js
+++ spec/models/gridSpec.js
@@ -4,4 +4,8 @@ describe('Grid', function() {
   it("exists", function() {
     expect(grid).toBeDefined();
   });
+
+  it("is initialized with a grid structure", function(){
+    expect(grid.structure).toEqual(['L1', 'M1', 'R1', 'L2', 'M2', 'R2', 'L3', 'M3', 'R3'])
+  });
 });
--- src/models/grid.js
+++ src/models/grid.js
@@ -1,6 +1,9 @@
 (function(exports) {
 
   function Grid() {
+    this.structure = [ 'L1', 'M1', 'R1',
+                       'L2', 'M2', 'R2',
+                       'L3', 'M3', 'R3' ];
   }
 
   exports.Grid = Grid;
