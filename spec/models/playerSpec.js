d848e3699e68b1293c6331d14579f30bffefb63d
Christos <chris.paraskeva10@gmail.com>
2017-04-21 21:07:35 +0100

    test passing movesMade being set to an empty array by default

--- spec/models/playerSpec.js
+++ spec/models/playerSpec.js
@@ -19,11 +19,15 @@ describe('Player', function() {
     expect(player.movesMade).toBeDefined();
   });
 
+  it("'movesMade' is set to an empty array by default", function(){
+    expect(player.movesMade).toEqual([]);
+  });
+
   it("is initialized with 'currentTurn' variable", function(){
     expect(player.currentTurn).toBeDefined();
   });
 
-  it("currentTurn is set to false by default", function(){
+  it("'currentTurn' is set to false by default", function(){
     expect(player.currentTurn).toEqual(false);
   });
 });
