cc1ba5e6db2155c833abe728b0a499496a8b2964
Christos <chris.paraskeva10@gmail.com>
2017-04-22 11:31:49 +0100

    change expectation of test in gridSpec

--- spec/models/gridSpec.js
+++ spec/models/gridSpec.js
@@ -6,6 +6,6 @@ describe('Grid', function() {
   });
 
   it("is initialized with a grid structure", function(){
-    expect(grid.structure).toEqual(['L1', 'M1', 'R1', 'L2', 'M2', 'R2', 'L3', 'M3', 'R3'])
+    expect(grid.structure).toEqual(resetGrid());
   });
 });
